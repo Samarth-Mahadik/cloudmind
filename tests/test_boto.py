276037c675c9d6adb0900f740803582f5c27968f
Samarth Mahadik <195641714+Samarth-Mahadik@users.noreply.github.com>
2025-09-30 16:43:50 +0530

    test_boto.py

--- tests/test_boto.py
+++ tests/test_boto.py
@@ -2,11 +2,12 @@
 import boto3
 from moto import mock_aws  # updated import for new Moto versions
 
+
 @mock_aws
 def test_get_caller_identity_mocked():
     sts = boto3.client("sts", region_name="us-east-1")
     identity = sts.get_caller_identity()
-    
+
     # Basic assertions
     assert "Account" in identity
     assert identity["Account"] is not None
